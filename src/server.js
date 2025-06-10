6557b9bbd615ede0b72aae30d641c2b590b218b3
ali-alrbidan <a30960314@gmail.com>
2025-06-11 00:28:50 +0300

    update speed

--- src/server.js
+++ src/server.js
@@ -3,14 +3,20 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import job from "./config/cron.js";
 
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+if (process.env.NODE_ENV === "production") job.start();
 // middleware
 app.use(express.json());
 app.use(rateLimiter);
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
 
 app.use("/api/transactions", transactionsRoute);
 
